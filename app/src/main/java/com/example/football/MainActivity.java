2c129d61e35f1324f35eff763a9222e7d7539dbf
Lamara2000 <mouzni.lamara@gmail.com>
2022-04-10 14:46:27 +0200

    test

--- app/src/main/java/com/example/football/MainActivity.java
+++ app/src/main/java/com/example/football/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.football;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#26A302"));
+        actionBar.setBackgroundDrawable(colorDrawable);
     }
 
     @Override
@@ -19,6 +29,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.match:
+                Intent i = new Intent(MainActivity.this, MatchActivity.class);
+                startActivity(i);
+                return (true);
+            case R.id.settings:
+                Intent i1 = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i1);
+                return (true);
+        }
         return true;
     }
 }
---
+++ app/src/main/java/com/example/football/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.example.football;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            Log.i("null","null");
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
